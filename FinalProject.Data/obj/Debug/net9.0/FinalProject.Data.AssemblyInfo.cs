8f94a83d90daa8586e4a46e44ff77698a28e7b93
Ahmedellaithy <125350354+Ahmedellaithy@users.noreply.github.com>
2025-02-06 06:24:59 +0200

    Add IIS

--- FinalProject.Data/obj/Debug/net9.0/FinalProject.Data.AssemblyInfo.cs
+++ FinalProject.Data/obj/Debug/net9.0/FinalProject.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e73d721765c052c8c62e9dfd23457ca03733076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de52463fe2fceac20f5aaae379202c34086a506")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
